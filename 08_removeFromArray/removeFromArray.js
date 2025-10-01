b39ea560cbdcbdfbb9b5557114cb6ed6dffc98e3
nuno-barroso <nuno_barroso6@hotmail.com>
2025-10-01 16:10:24 +0100

    js 08 exercise complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    return arr.filter((element) => !(elements.includes(element)));
 };
 
+console.log(removeFromArray([1, 2, 2, 4], 2));
 // Do not edit below this line
 module.exports = removeFromArray;
