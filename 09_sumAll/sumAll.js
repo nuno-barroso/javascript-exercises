9ce29f20d7b6222a37be28472531ebfd31d738b9
nbarroso <n.barroso@campus.fct.unl.pt>
2025-10-01 17:05:18 +0100

    js 09 exercise complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !(Number.isInteger(a)) || !(Number.isInteger(b)) || isNaN(a) || isNaN(b)) {
+        return "ERROR";
+    }
 
+    if (a > b) {
+        let aux = 0;
+        aux = b;
+        b = a;
+        a = aux;
+    }
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
