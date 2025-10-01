72b1179e47c450c24308d5f89b541a1e87a33a3a
nuno-barroso <nuno_barroso6@hotmail.com>
2025-10-01 12:35:20 +0100

    js 07 exercise complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
-
+console.log(reverseString("Hello there"));
 // Do not edit below this line
 module.exports = reverseString;
