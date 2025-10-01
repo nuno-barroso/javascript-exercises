27296899480e285fd916a8fdaf7dfd72c3b165fd
nbarroso <n.barroso@campus.fct.unl.pt>
2025-10-01 17:35:50 +0100

    js 11 exercise complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = 0;
+  celsius = (temp - 32) * (5/9);
+  return Math.round(celsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fah = 0;
+  fah = (temp * (9/5) + 32);
+  return Math.round(fah * 10) / 10; 
 };
 
 // Do not edit below this line
